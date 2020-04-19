af696ab8bc300ce764685b50753482b3ee740b98
Monse Salas <cmonse.salasv@gmail.com>
2020-04-19 18:24:47 -0500

    tutorial to do list

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export default{
+        apiKey: "AIzaSyAgjWxoKv2OjScBF5ukEKTKjSAaSIfU5io",
+        authDomain: "lab-notes-a328f.firebaseapp.com",
+        databaseURL: "https://lab-notes-a328f.firebaseio.com",
+        projectId: "lab-notes-a328f",
+        storageBucket: "lab-notes-a328f.appspot.com",
+        messagingSenderId: "1099222843115",
+        appId: "1:1099222843115:web:9e9ee43865554d5aa308ae",
+        measurementId: "G-SK0D5EPP5F"
+}
