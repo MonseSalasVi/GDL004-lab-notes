b73da04a8a2d1c5cfaf9b9d2afc84844db715d52
MonseSalasVi <cmonse.salasv@gmail.com>
2020-04-25 20:52:49 -0500

    router

--- src/index.js
+++ src/index.js
@@ -1,43 +1,16 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import routes from "./routers";
-import Header from "./Header";
 import "./index.css";
-
 import * as firebase from "firebase";
 import firebaseConfig from "./firebase.config";
+import App from './app.js';
 
 firebase.initializeApp(firebaseConfig);
 
-export const AuthContext = React.createContext(null);
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
 
-function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
 
 
-  return (
-    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
-      Is logged in? {JSON.stringify(isLoggedIn)}
-      <div className="App">
-        <Router>
-          <Header />
-
-          <Switch>
-            {routes.map(route => (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.main}
-              />
-            ))}
-          </Switch>
-        </Router>
-      </div>
-    </AuthContext.Provider>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
